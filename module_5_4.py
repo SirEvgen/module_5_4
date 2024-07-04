16adaf0131cd1eaf4b8903132ff4c38779dff8dd
SirEvgen <sandrofa6666666@gmail.com>
2024-07-04 18:02:52 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,59 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return super().__new__(cls)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
+    def __init__(self, name, number_of_floor):
+        self.name = name
+        self.number_of_floor = number_of_floor
+
+    def __len__(self):
+        return self.number_of_floor
+
+    def __eq__(self, other):
+        return self.number_of_floor == other.number_of_floor
+
+    def __lt__(self, other):
+        return self.number_of_floor < other.number_of_floor
+
+    def __le__(self, other):
+        return self.number_of_floor <= other.number_of_floor
+
+    def __gt__(self, other):
+        return self.number_of_floor > other.number_of_floor
+
+    def __ge__(self, other):
+        return self.number_of_floor >= other.number_of_floor
+
+    def __ne__(self, other):
+        return self.number_of_floor != other.number_of_floor
+
+    def __add__(self, other):
+        self.number_of_floor = self.number_of_floor + other
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        self.number_of_floor += other
+        return self
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floor}'
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
